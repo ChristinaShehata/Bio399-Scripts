f8394789052bc2940e62e22f588e7a0411c2b0b6
Christina Shehata <christinashehata2019@u.northwestern.edu>
2018-04-18 12:09:33 -0500

    call function

--- FASTAtoPHYLIP.py
+++ FASTAtoPHYLIP.py
@@ -1,13 +1,11 @@
 import sys
 from Bio import AlignIO
 
-def convert(sys.argv[1], sys.argv[2]):
+def convert(fasta, phylip):
 	""" converts a FASTA file into a PHYLIP file
-	input sys.argv[1]: input fasta file containing sequences of equal length
-	input sys.argv[2]: output phylip file
+	input fasta: input fasta file containing sequences of equal length
+	input phylip: output phylip file
 	"""
-	fasta = sys.argv[1]
-	phylip = sys.argv[2]
 	input_handle = open(fasta, "rU")
 	output_handle = open(phylip, "w")
 
@@ -16,3 +14,7 @@ def convert(sys.argv[1], sys.argv[2]):
 
 	output_handle.close()
 	input_handle.close()
+
+convert(sys.argv[1], sys.argv[2])
+		
+		
