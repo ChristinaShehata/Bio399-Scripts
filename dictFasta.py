b761ff8bfd0ae929ef474e52681df9731c36754e
Christina Shehata <christinashehata2019@u.northwestern.edu>
2018-05-02 10:48:48 -0500

    works for small sample haploDictA

--- dictFasta.py
+++ dictFasta.py
@@ -1,6 +1,12 @@
-#haploDictA = {60181: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60219: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60220: {'NA18861A': 'A', 'HG02493A': 'A', 'NA11918A': 'A'}, 60278: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60317: {'NA18861A': 'C', 'HG02493A': 'C', 'NA11918A': 'C'}}
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
-def fasta(n):
+haploDictA = {60181: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60219: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60220: {'NA18861A': 'A', 'HG02493A': 'A', 'NA11918A': 'A'}, 60278: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60317: {'NA18861A': 'C', 'HG02493A': 'C', 'NA11918A': 'C'}}
+
+windowList = list(range(55000, 105000, 5000))
+
+def parseDict(n):
 	refList = ['-']*10000
 	newSeq = ''
 	tupList = []
@@ -8,30 +14,39 @@ def fasta(n):
 		for i in range(len(refList)):
 			if position - n == i:
 				for id in id_snp.keys():
-					newList = [refList]
-					for rList in newList:
-						rList[i] = id_snp[id]
-						newSeq = "".join(list(rList))
-						tup = (id, newSeq)
-						tupList.append(tup)
-	return tupList
+					refList[i] = id_snp[id]
+					newSeq = "".join(refList)
+					tup = (id, newSeq)
+					tupList.append(tup)
+	return tupList[-3:] #3 = number of individuals -- contains all the snps in one window
 
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+        
 def prep():
-	windowList = list(range(55000, 105000, 5000))
 	new_dict = {}
-	fa = ""
+	recordList = []
 	for n in windowList:
-		L = [[fasta(n), n] for n in windowList] #makes all windows for one individual
-	#print(L)
-	for seq in L:
-		if seq[0] != ():
-			new_dict[(str(seq[1]) + "-" + str(seq[1]+10000))] = SeqRecord(Seq(seq[0][1]),id=seq[0][0])
+		for x in parseDict(n):
+			if x[1] != ():
+				recordList.append(SeqRecord(Seq(x[1]),id=x[0],name=str(n)))
+	rList = list(chunks(recordList, 3)) #individuals
+	for r in rList:
+		for record in r:
+			new_dict[record.name] = r
 	return new_dict
 
-
 def write(new_dict):
-	with open("/Users/ChristinaShehata/Desktop/{}.fa".format(id), "w") as f:
-		for record in new_dict.keys():
-			f.write("{}	{}\n".format(("> " + new_dict[record].id + ", Window: " + record), new_dict[record].seq))
+	for record, seqList in new_dict.items():
+		with open("/Users/ChristinaShehata/Desktop/ouput{}.fa".format(record + '-' + str(int(record)+10000)), "w") as f:
+			for ind_seq in seqList:
+				f.write("{}	{}\n".format(("> " + ind_seq.id), ind_seq.seq))
 
-write(prep())
+def main():
+	dictPrep = prep()
+	write(dictPrep)
+
+main()
+
+
