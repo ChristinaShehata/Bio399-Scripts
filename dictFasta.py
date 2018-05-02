fef7a3707253021215b6b57132eaa48a523dbfc4
Christina Shehata <christinashehata2019@u.northwestern.edu>
2018-05-02 10:57:18 -0500

    added docstrings and magic numbers

--- dictFasta.py
+++ dictFasta.py
@@ -4,10 +4,16 @@ from Bio.SeqRecord import SeqRecord
 
 haploDictA = {60181: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60219: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60220: {'NA18861A': 'A', 'HG02493A': 'A', 'NA11918A': 'A'}, 60278: {'NA18861A': 'G', 'HG02493A': 'G', 'NA11918A': 'G'}, 60317: {'NA18861A': 'C', 'HG02493A': 'C', 'NA11918A': 'C'}}
 
-windowList = list(range(55000, 105000, 5000))
+windowLength = 10000
+overlapLength = 5000
+sampleCount = 3		# number of individuals
+windowList = list(range(55000, 105000, overlapLength))
 
 def parseDict(n):
-	refList = ['-']*10000
+	""" parseDict parses through haploDictsA and B
+	input n: start position of window
+	"""
+	refList = ['-']*windowLength
 	newSeq = ''
 	tupList = []
 	for position, id_snp in haploDictA.items():
@@ -18,28 +24,31 @@ def parseDict(n):
 					newSeq = "".join(refList)
 					tup = (id, newSeq)
 					tupList.append(tup)
-	return tupList[-3:] #3 = number of individuals -- contains all the snps in one window
+	return tupList[-sampleCount:] #3 = number of individuals -- contains all the snps in one window
 
 def chunks(l, n):
+	""" chunks separates a list into even-sized parts """
     for i in range(0, len(l), n):
         yield l[i:i+n]
         
 def prep():
+	""" prep makes a dictionary with fasta file information """
 	new_dict = {}
 	recordList = []
 	for n in windowList:
 		for x in parseDict(n):
 			if x[1] != ():
 				recordList.append(SeqRecord(Seq(x[1]),id=x[0],name=str(n)))
-	rList = list(chunks(recordList, 3)) #individuals
+	rList = list(chunks(recordList, sampleCount)) #individuals
 	for r in rList:
 		for record in r:
 			new_dict[record.name] = r
 	return new_dict
 
 def write(new_dict):
+	""" write puts sequences into fasta format """
 	for record, seqList in new_dict.items():
-		with open("/Users/ChristinaShehata/Desktop/ouput{}.fa".format(record + '-' + str(int(record)+10000)), "w") as f:
+		with open("/Users/ChristinaShehata/Desktop/ouput{}.fa".format(record + '-' + str(int(record)+windowLength)), "w") as f:
 			for ind_seq in seqList:
 				f.write("{}	{}\n".format(("> " + ind_seq.id), ind_seq.seq))
 
