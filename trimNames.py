3e8cdf47c35958194ee125f2ef9ed77cfdf59af6
Christina Shehata <christinashehata2019@u.northwestern.edu>
2018-05-16 19:09:21 -0500

    docstrings

--- trimNames.py
+++ trimNames.py
@@ -1,7 +1,18 @@
 import sys
 from Bio import SeqIO
 
+####################################################################################
+##
+## Files Needed:
+## sys.argv[1] = fasta with full names
+## sys.argv[2] = fasta with abbreviated names
+##
+####################################################################################
+
 def trimName(fasta):
+	""" cuts down sample names to convert into phylip format
+	input fasta: fasta file of trimmed alignments
+	"""
 	newDict = {}
 	with open(fasta, 'r') as f:
 		records = list(SeqIO.parse(f, "fasta"))
@@ -10,6 +21,7 @@ def trimName(fasta):
 	return newDict
 
 def write(newDict, cutFasta):
+	""" writes trimmed names and sequences """
 	with open(cutFasta, 'w') as f:
 		for id, seq in newDict.items():
 			f.write('>'+id+'\n')
