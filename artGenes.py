51f4239d07a12991c4304282c6fc0321194d89e1
Christina Shehata <christinashehata2019@u.northwestern.edu>
2018-05-27 22:56:58 -0500

    from baits file

--- artGenes.py
+++ artGenes.py
@@ -1,44 +1,20 @@
-# coding: utf-8
 import sys
 from Bio import SeqIO
-mypath = '/Users/ChristinaShehata/Desktop/assemblies/' #path to assemblies
-#mypath = '/artocarpus/lauren/Hybpiper/assemblies/'
 
-def sampleList(indList):
-	"""
-	sampleList creates a list out of a text file containing individuals of interest
-	input indList: a list containing the individuals of interest separated by '\n'
-	"""
-	with open(indList, "r") as f: 
-		L = [line.strip() for line in f.readlines()]
-	return L
+####################################################################################
+##
+## sys.argv[1] = baits file
+## sys.argv[2] = output file
+##
+####################################################################################
 
-def get_genes(sampList, gene):
-	""" get_genes opens up folders for individuals of interest and parses for
-	a particular gene of interest
-	input sampList: list of individuals of interest
-	input gene: gene of interest
-	"""
-	geneDict = {}
-	for ind in sampList:
-		with open(mypath + "{}/{}/{}_contigs.fasta".format(ind,gene,gene), 'r') as f:
-			records = list(SeqIO.parse(f, "fasta"))
-			geneDict[ind] = ('> ' + ind, records[0].seq) #assuming 1st node?
-	return geneDict
-
-def write(geneDict, outFile):
-	""" writes gene sequences for each individual into one fasta file
-	input geneDict: dictionary of each individual's sequence for a particular gene
-	input outFile: output fasta file
-	"""
+def get_genes(baits_file, outFile):
+	""" makes geneList from baits file """
 	with open(outFile, 'w') as f:
-		for id, seq in geneDict.items():
-			f.write(seq[0] +'\n')
-			f.write(str(seq[1]) + '\n')
+		records = list(SeqIO.parse(baits_file, 'fasta'))
+		for record in records:
+			f.write(record.id[11:]+'\n')
 
-def main():
-	sampList = sampleList(sys.argv[2])
-	geneDict = get_genes(sampList, sys.argv[1])
-	write(geneDict, sys.argv[3])
-	
-main()
+get_genes('/Users/ChristinaShehata/Desktop/newref_camansi.fna', '/Users/ChristinaShehata/Desktop/artGeneList.txt')
+
+
