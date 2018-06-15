9cea80c181ba04d1cece185d4e2a13e3b3b77ab5
Christina Shehata <christinashehata2019@u.northwestern.edu>
2018-06-15 11:37:35 -0500

    added sys, docstrings - works with modelA1 files

--- parsePAML.py
+++ parsePAML.py
@@ -1,26 +1,45 @@
 import sys
 
-paml_results_directory = '/Users/ChristinaShehata/Desktop/PAML_results_modelA1/'
-omega_list = []
+######################################################################
+## 
+## Files needed:
+## sys.argv[1] is text file containing gene list
+## sys.argv[2] is path to directory containing output PAML files
+## sys.argv[3] is output file
+##
+######################################################################
 
-def geneList():
-	with open('/Users/ChristinaShehata/Desktop/artGeneList.txt', 'r') as f:
+def geneList(geneText):
+	""" makes a list of genes """
+	with open(geneText, 'r') as f:
 		genes = [line.strip() for line in f.readlines()]
 		return genes
 
-def parsePAML(genes):
+def parsePAML(genes, paml_results_directory):
+	""" parses through PAML output to get omega
+	input genes: list of genes
+	"""
+	omega_list = []
 	for gene in genes:
 		with open(paml_results_directory + '{}.paml'.format(gene)) as f:
 			read = f.readlines()
 			for line in read:
 				if line.startswith('Model'):
 					x = line.split(' ')
-					omega_list.append(x[-2])
+					omega_list.append((gene, x[-2]))
+	return omega_list
+
+def write(omega_list, outFile):
+	""" write results to a text file """
+	with open(outFile, 'w') as f:
+		for x in omega_list:
+			f.write(x[0] + '\t' + x[1])
+			f.write('\n')
 
 def main():
-	genes = geneList()
-	parsePAML(genes)
-	print(omega_list)
+	genes = geneList(sys.argv[1])
+	omega_list = parsePAML(genes, sys.argv[2])
+	write(omega_list, sys.argv[3])
 
 main() #works with ModelA1 files
 			
