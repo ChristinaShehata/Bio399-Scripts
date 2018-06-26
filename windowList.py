544c072728d0a41f296e4de0d89cfac06a2821a6
Christina Shehata <christinashehata2019@u.northwestern.edu>
2018-06-26 10:57:52 -0500

    first commit

---
+++ windowList.py
@@ -0,0 +1,19 @@
+import sys
+
+start = 111350000
+end = 111460000
+overlapLength = 5000
+
+windowList = list(range(start, end, overlapLength))
+
+def windowList(outFile):
+	""" makes a list of loci windows
+	input outFile: text list of loci
+	"""
+	with open(outFile, 'w') as f:
+		for n in windowList:
+			f.write(str(n) + '\n')
+	
+def main():
+	windowList(sys.argv[1])
+
