32c29c6d4d4294504aa588fb4338b42cdc667d82
Christina Shehata <christinashehata2019@u.northwestern.edu>
2018-08-03 17:06:36 -0500

    first commit

---
+++ makePopulationFiles.py
@@ -0,0 +1,79 @@
+import sys
+import csv
+from Bio import SeqIO
+
+####################################################################################
+##
+## Files Needed:
+## sys.argv[1] = list of all genes
+## sys.argv[2] = csv containing tree information for population file
+## sys.argv[3] = path to directory containing trimmed alignments
+## sys.argv[4] = path to directory containing output population files
+## sys.argv[5] = number of branches to test
+## sys.argv[6] = output n File (number of taxa per gene tree)
+##
+####################################################################################
+
+def allGenes(geneFile):
+	""" makes master gene List """
+	with open(geneFile, 'r') as f:
+		geneList = [line.rstrip() for line in f.readlines()]
+	return geneList
+
+def master_pop(inFile):
+	""" makes dictionary of all population information for each individual """
+	master_popDict = {}
+	with open(inFile, 'r', encoding="utf-8") as input:
+		reader = csv.reader(input)
+		for record in reader:
+			master_popDict[record[0].replace('\ufeff', '')] = record
+	for value in master_popDict.values():
+		for i in range(len(value)):
+			if value[i] == '0': value[i] = '00'
+	return master_popDict
+
+def ind_geneDict(gene, myPath):
+	""" makes a dictionary containing the individuals represented in each gene file """
+	ind_geneDict = {}
+	recordList = []
+	with open(myPath + '{}.trimmed.FNA.nog'.format(gene), 'r') as f:
+		for record in SeqIO.parse(f, "fasta"):
+			recordList.append(record.id)	
+	ind_geneDict[gene] = recordList
+	return ind_geneDict
+
+def write_popFile(master_popDict, indGeneDict, gene, branches, outFile):
+	""" writes population file """
+	with open(outFile, 'w') as f:
+		for i in range(int(branches)+1):
+			for ind in master_popDict:
+				if ind in indGeneDict[gene]:
+					f.write(master_popDict[ind][i].replace('\ufeff', '') + '\t')
+			f.write('\n')
+
+def write_nFile(indGeneDict, outFile):
+	with open(outFile, 'w') as f:
+		for value in indGeneDict.values():
+			f.write(str(len(value)) + '\n')
+
+def main():
+	branches = sys.argv[5]
+	dictList = []
+	geneList = allGenes(sys.argv[1])
+	master_popDict = master_pop(sys.argv[2])
+	for gene in geneList:
+		dictList.append(ind_geneDict(gene, sys.argv[3]))
+	indGeneDict = {k: v for d in dictList for k, v in d.items()}
+	for gene in geneList:
+		write_popFile(master_popDict, indGeneDict, gene, branches, sys.argv[4]+'{}.pops'.format(gene))
+	write_nFile(indGeneDict, sys.argv[6])
+	
+main()
+
+
+
+
+
+
+
+			
